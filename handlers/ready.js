6fd225067cb7a0798ef87dfabdf313551830863e
pottteryyy <49904492+pottteryyy@users.noreply.github.com>
2019-11-23 16:44:42 +0700

    Add files via upload

---
+++ handlers/ready.js
@@ -0,0 +1,29 @@
+const config = require("./config/musicbot.json");
+
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is ready for action!`);
+
+    if (config.activity.streaming == true) {
+
+        bot.user.setActivity(config.activity.game, {
+
+            url: 'https://twitch.tv/username'
+
+        });
+
+    } else {
+
+        bot.user.setActivity(config.activity.game, {
+
+            type: 'WATCHING'
+
+        }); //PLAYING, LISTENING, WATCHING
+
+        bot.user.setStatus('dnd'); // dnd, idle, online, invisible
+
+    }
+
+});
